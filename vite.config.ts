f5c6d6fa2fe3bc4fae83332af2bce244ebc246cc
noahdot <mayor20xx@gmail.com>
2024-12-20 02:14:24 +0800

    chore(dependencies, build): add new dependencies and update vite config
    added commitizen, cz-conventional-changelog, unplugin-auto-import, unplugin-vue-components and
    updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import path from 'path';
+
+const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        imports: ['vue'],
+        eslintrc: {
+          enabled: false,
+          filepath: './.eslintrc-auto-import.json'
+        },
+        dts: path.resolve(pathSrc, 'types', 'auto-imports.d.ts')
+      }),
+      Components({
+        dts: path.resolve(pathSrc, 'types', 'components.d.ts')
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': pathSrc,
+      }
+    },
+  }
+});
