ad6ef08adb025689c15fa9cee8f429d68518f7f2
noahdot <mayor20xx@gmail.com>
2024-12-20 14:24:00 +0800

    feat(element-plus): integrate Element Plus and update vite.config.ts for Elemenet Plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,43 @@
-import { defineConfig } from 'vite'
+import { type ConfigEnv, type UserConfig, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 import path from 'path';
 
 const pathSrc = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
   return {
     plugins: [
       vue(),
       AutoImport({
         imports: ['vue'],
+        resolvers: [
+          ElementPlusResolver(),
+          IconsResolver({})
+        ],
         eslintrc: {
           enabled: false,
           filepath: './.eslintrc-auto-import.json'
         },
+        vueTemplate: true,
         dts: path.resolve(pathSrc, 'types', 'auto-imports.d.ts')
       }),
       Components({
+        resolvers: [
+          ElementPlusResolver(),
+          IconsResolver({
+            enabledCollections: ['ep']
+          })
+        ],
         dts: path.resolve(pathSrc, 'types', 'components.d.ts')
+      }),
+      Icons({
+        autoInstall: true
       })
     ],
     resolve: {
